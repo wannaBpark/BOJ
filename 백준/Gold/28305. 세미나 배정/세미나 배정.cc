9a9db1f182e6524c6dcb17db0613dc1d07843fd2
wannaBpark <leehun456@hanmail.net>
2023-07-06 18:04:37 +0900

    [Gold II] Title: 세미나 배정, Time: 208 ms, Memory: 7272 KB -BaekjoonHub

--- 백준/Gold/28305. 세미나 배정/세미나 배정.cc
+++ 백준/Gold/28305. 세미나 배정/세미나 배정.cc
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
+#define DEBUG 1
 using namespace std;
 int N,T;
 vector<int> v;
-bool affordable(int sz)
+bool affordable(int sz) // 최대로 겹치는 세미나 수를 sz개 이하로 배정 가능?
 {
-	bool ret = true;
 	priority_queue<pair<int, int>, vector<pair<int,int>> , greater<pair<int,int> > > pq;
 	int start = max(1, v[0] - T + 1); 
 	int end = start + T - 1;
@@ -13,17 +13,21 @@ bool affordable(int sz)
 		int& cur = v[i];
 		start = max(1, cur - T + 1);
 		if (pq.size() >= sz) {
+			// mn : 맨 앞의 세미나의 끝나는 날짜 + 1
+			// = 현재 cur인 세미나가 시작할 수 있는 가장 작은 수
+			// mn이 현재 배정하려는 cur보다 크다면
+			// 배정할 수 없다는 뜻 (세미나에 외부강사 초청 불가)
 			int mn = pq.top().second + 1;
-			start = max(start, mn);
+			start = max(start, mn);	
 			if (mn > cur) {
-				return ret = false;
+				return false;
 			}
 			pq.pop();
-        }
+		}
 		end = start + T - 1;
 		pq.push(make_pair(start, end));
 	}
-	return ret;
+	return true;
 }
 int main()
 {
@@ -35,11 +39,13 @@ int main()
 	}
 	sort(v.begin(), v.end());
 
-	int ans = 2e9;
-	int l = 1, r = 1e9;
+	int ans = 1e9+1;
+	int l = 1, r = 200000;
 
+	//bool res = affordable(2);
 	while (l <= r) {
 		int mid = (l + r) / 2;
+
 		bool res = affordable(mid);
 		if (res == true) {
 			r = mid - 1;
