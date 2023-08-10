27c972572f3b45c5c7c1dcd624f7624233355ab8
wannaBpark <leehun456@hanmail.net>
2023-08-10 17:01:08 +0900

    [Platinum IV] Title: 사탕 줍는 로봇, Time: 4 ms, Memory: 4984 KB -BaekjoonHub

---
+++ 백준/Platinum/15892. 사탕 줍는 로봇/사탕 줍는 로봇.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+using namespace std;
+const int MAX_V = 600;
+const int INF = 1e9;
+
+int N, M;                  // 소의 수 N, 축사의 수 M
+int c[MAX_V][MAX_V] = {0}; // c[i][j]: i에서 j로 가는 간선의 용량
+int f[MAX_V][MAX_V] = {0}; // f[i][j]: i에서 j로 현재 흐르는 유량
+vector<int> adj[MAX_V];    // 인접 리스트
+
+// total: 총 유량, S: 소스, E: 싱크
+int total = 0, S = 1, E = 2;
+
+void bfs_Edmonds_Karp()
+{
+    // 증가 경로를 못 찾을 때까지 루프
+    while (true) {
+        // 증가 경로를 BFS로 찾음
+        int prev[MAX_V];
+        fill(prev, prev + MAX_V, -1);
+        queue<int> Q;
+        Q.push(S);
+        while (!Q.empty() && prev[E] == -1) {
+            int cur = Q.front();
+            Q.pop();
+            for (int next : adj[cur]) {
+                // c[i][j]-f[i][j] > 0: i에서 j로 유량을 흘릴 여유가 남았는가?
+                // prev[next] == -1: next 정점을 아직 방문하지 않았는가?
+                if (c[cur][next] - f[cur][next] > 0 && prev[next] == -1) {
+                    Q.push(next);
+                    prev[next] = cur; // 경로를 기억하기 위해 prev 배열 사용
+                    if (next == E) break; // 싱크에 도달하면 나옴
+                }
+            }
+        }
+        // 싱크로 가는 경로가 더 없으면 루프 탈출
+        if (prev[E] == -1) return;
+
+        // 경로상에서 가장 허용치가 낮은 곳을 찾음
+        int flow = INF;
+        for (int i = E; i != S; i = prev[i]) {
+            int& from = prev[i];
+            int& to = i;
+            flow = min(flow, c[from][to] - f[from][to]);
+        }
+        // 찾은 flow만큼 해당 경로에 유량 흘려줌
+        for (int i = E; i != S; i = prev[i]) {
+            int& from = prev[i];
+            int& to = i;
+            f[from][to] += flow;
+            f[to][from] -= flow;
+        }
+        // 총 유량 값 증가
+        total += flow;
+    }
+}
+
+int main()
+{
+    fastio;
+    // 간선 정보 입력받기
+    cin >> N >> M;
+    for (int i = 0; i < M; ++i)
+    {
+        int u,v,w;
+        cin >> u >> v >> w;
+        c[u][v] += w; // 같은 간선이 여러 번 들어올 수 있으므로 +=
+        c[v][u] += w;
+        adj[u].push_back(v);
+        adj[v].push_back(u); // 역방향 간선도 추가해줘야 함
+    }
+    E = N;
+    bfs_Edmonds_Karp();
+
+    // 결과 출력
+    cout << total;
+}
