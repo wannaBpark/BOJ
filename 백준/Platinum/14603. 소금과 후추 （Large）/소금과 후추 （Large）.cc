3bcea5f4e12baf0474699e7879aa0681e0b0f862
wannaBpark <leehun456@hanmail.net>
2023-08-17 19:59:03 +0900

    [Platinum I] Title: 소금과 후추 (Large), Time: 184 ms, Memory: 3552 KB -BaekjoonHub

--- 백준/Platinum/14603. 소금과 후추 （Large）/소금과 후추 （Large）.cc
+++ 백준/Platinum/14603. 소금과 후추 （Large）/소금과 후추 （Large）.cc
@@ -2,13 +2,8 @@
 #define MX_N 310
 #define MX_K 1e5
 #define fastio cin.tie(0)->sync_with_stdio(0)
-#define x first
-#define y second
 using namespace std;
-
 using ll = long long;
-using ld = long double;
-using pii = pair<ll, ll>;
 
 int Fenwick_Tree[100020];
 
@@ -17,7 +12,6 @@ int median;
 int M, N, K, W;
 int arr[MX_N][MX_N];
 int cnt[100010];
-// int tree[MX_K];
 int ans[MX_N][MX_N];
 
 int Sum(int idx)
@@ -38,52 +32,35 @@ void Update(int Idx, int Value)
     }
 }
 
-inline void DeletePrevCol(int r, int c)
-{
+inline void DeletePrevCol(int r, int c) {
     for (int i = 0; i < W; ++i) {
         int& val = arr[r + i][c];
-
         Update(val, -1);    
     }
 }
 
-inline void DeletePrevRow(int r, int c)
-{
-    // cout << "-------------r c " << r << " " << c<<endl;
-
+inline void DeletePrevRow(int r, int c) {
     for (int j = 0; j < W; ++j) {
         int& val = arr[r][c + j];
-        // cout << val << endl;
-
         Update(val, -1);
     }
 }
 
-inline void AddCurCol(int r, int c) 
-{
-    // cout << "-------------r c " << r << " " << c<<endl;
+inline void AddCurCol(int r, int c) {
     for (int i = 0; i < W; ++i) {
         int& val = arr[r + i][c];
-
-        // cout << val << endl;
         Update(val, 1);    
     }
 }
 
-inline void AddCurRow(int r, int c)
-{
-    // cout << "-------------r c " << r << " " << c<<endl;
-
+inline void AddCurRow(int r, int c) {
     for (int j = 0; j < W; ++j) {
         int& val = arr[r][c + j];
-        // cout << val << endl;
-
         Update(val, 1);
     }
 }
 
-inline bool InRange(int r)
-{
+inline bool InRange(int r) {
     return (r + W - 1 < M);
 }
 
@@ -92,7 +69,6 @@ inline int bsearch()
     int lo = 1;
     int hi = MX_INPUT + 2;
     int best = MX_K;
-    // cout << "bsearch" << endl;
     while (lo <= hi) {
         int mid = (lo + hi) / 2;
         int res = Sum(mid);
@@ -102,12 +78,11 @@ inline int bsearch()
         } else {
             lo = mid + 1;
         }
-        // cout << lo << " " << hi << " " << res << " median : " << median<< endl;
-
     }
     return best;
 }
 
+// 0 ~ K 까지 범위 유의
 int main()
 {
     fastio;
@@ -125,62 +100,45 @@ int main()
     for (int i = 0; i < W; ++i) {
         for (int j = 0; j < W; ++j) {
             int& val = arr[i][j];
-            // cnt[val]++;
             Update(val, 1);
         }
     }
 
-    // for (int i = 1; i <= 10; ++i) {
-    //     cout << Fenwick_Tree[i] << " ";
-    // }
-    // cout << endl << "Sum " << endl;
-    // for (int i = 1; i <= 10; ++i) {
-    //     cout << Sum(i) << " ";
-    // }
-    // cout << endl;
-
     median = (W*W + 1) / 2;
     int row = 0, col = 0;
-    while (true) {
-        
-        // if (row == M - W && col == N - W) break;
-        
+    while (true) {        
         if (!InRange(row)) break;
 
         if (row != 0) {
             DeletePrevRow(row - 1, 0);
             AddCurRow(row + W - 1, 0);
         }
-        // -> 탐색
+        // 오른쪽으로 탐색 (행 고정)
         for (col = 0; col <= N - W; ++col) {
             if (col != 0) {   
                 DeletePrevCol(row, col - 1);
                 AddCurCol(row, col + W - 1);  
             }
             ans[row][col] = bsearch();
-            // cout << row << " " << col << endl;
         }
+
         ++row;
-        --col;
         if (!InRange(row)) break;
         
         DeletePrevRow(row - 1, N - W);
         AddCurRow(row + W - 1, N - W);
         
-        // <- 탐색
+        /// 왼쪽으로 탐색 (행 고정)
         for (col = N - W; col >= 0; --col) {
             if (col != N - W) {
                 DeletePrevCol(row, col + W);
                 AddCurCol(row, col);
             }
             ans[row][col] = bsearch();
-            // cout << row << " " << col << endl;
         }
         ++row;
-        ++col;
     }
 
-    // cout << endl << "Output" << endl;
     for (int i = 0; i <= M - W; ++i) {
         for (int j = 0; j <= N - W; ++j) {
             cout << ans[i][j] - 1 << " ";
