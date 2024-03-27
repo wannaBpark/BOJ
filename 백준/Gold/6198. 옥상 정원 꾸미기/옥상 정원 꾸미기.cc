67e826b63bdc8d0a19429cfb37169eda74ee6e2b
wannaBpark <leehun456@hanmail.net>
2024-03-27 20:20:53 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 12 ms, Memory: 3040 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -2,87 +2,38 @@
 using namespace std;
 using ll = long long;
 
-static inline ll max(ll a, ll b)
-{
-    return a >= b ? a : b;
-}
-
 stack<int> st;
 ll h[80010];
-ll sum[80010];
-ll ans = 0;
-ll ans2 = 0;
-ll tmp;
+ll ans = 0, tmp;
 int n, i;
 int main()
 {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    cout.tie(NULL);
+    cin.tie(NULL); ios_base::sync_with_stdio(false); cout.tie(NULL);
     cin >> n;
-    for (i = 0; i < n; ++i)
-        cin >> h[i];
+    for (i = 0; i < n; ++i) cin >> h[i];
 
     for (i = 0; i < n; ++i)
     {
-        auto cur = i;
         tmp = 0;
         while (!st.empty() && h[st.top()] <= h[i])
         {
             tmp++;
-            ll curLowest = st.top();
             st.pop();
-            ll curTop = (st.empty()) ? 0 : st.top();
-
-            ll cnt = curLowest - curTop; //Gurantee it's all monotonic low before cur
-
-            ans += cnt;
-
-            sum[curTop] += 1;
-
-            
-            // cout << "curTop and Low : " << curTop <<" " << curLowest<< " " << ans<< "\n";
-            // cout << "i : " << cur <<" " <<cnt << " " << ans<< "\n";
         }
-        if (st.empty()) {
-            tmp -= 1;
-            ans2 += (tmp + 1) * tmp / 2;  
-        } else {
-            ans2 += ll(st.size()) * tmp;
-            ans2 += (tmp - 1) * tmp / 2;
-            // if(tmp>0) 
-            // cout << "SIZE: " << st.size() << endl;
-        }
-              
         
-        // cout << "i: " << i <<" TMP : " << tmp << " ANS2 : " << ans2 << endl;
-        st.push(cur);
+        if (!st.empty()) {
+            ans += ll(st.size()) * tmp;
+        }
+        ans += (tmp - 1) * tmp / 2;
+        
+        st.push(i);
     }
-
-    // for (int i = 0; i < n; ++i) {
-    //     printf("%d ", sum[i]);
-    // }
-    // puts("");
     tmp = 0;
     while (!st.empty()) {
-        ll curLowest = st.top();
         st.pop();
-        ll curTop = (st.empty()) ? 0 : st.top();
-
-        ll cnt = curLowest - curTop; //Gurantee it's all monotonic low before cur
-
-        ans += cnt;
-        sum[curTop] += 1;
         ++tmp;
-        
-        // cout << "curTop and Low : " << curTop <<" " << curLowest<< " " << ans<< "\n";
-
     }
-    ans2 += (tmp - 1) * tmp / 2;
-    // cout << "TMP : " << tmp << " ANS2 : " << ans2 << endl;
-    
-
-    cout << ans2;
-
+    ans += (tmp - 1) * tmp / 2;
+    cout << ans;
     return 0;
 }
