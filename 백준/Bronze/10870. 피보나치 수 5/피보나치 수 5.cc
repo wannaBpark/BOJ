fe0df29ca365bbd2570027c8845c2a977b5c6749
wannaBpark <leehun456@hanmail.net>
2024-04-11 16:44:45 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int Fibo(int n)
+{
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+    
+    return Fibo(n-1) + Fibo(n-2);
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", Fibo(n));
+    return 0;
+}
