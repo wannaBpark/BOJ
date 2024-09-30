d439e4e20ba3238ce53677b083e85d75b77db251
wannaBpark <leehun456@hanmail.net>
2024-09-30 22:59:55 +0900

    [Silver V] Title: 숫자 카드, Time: 288 ms, Memory: 3068 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int A[500001];
+int bsearch(int start, int end, int k)
+{
+	int mid = (start + end) / 2;
+	if (start > end) return 0;
+	if (A[mid] == k) return 1;
+	else if (A[mid] < k)
+		return bsearch(mid + 1, end, k);
+	else
+		return bsearch(start, mid - 1, k);
+}
+int main()
+{
+	int N, M, a, i = 0;
+	scanf("%d", &N);
+	while (N--) scanf("%d", &A[i++]);
+	sort(A, A + i);
+	scanf("%d", &M);
+	while (M--) scanf("%d", &a), printf("%d ", bsearch(0, i - 1, a));
+	return 0;
+}
