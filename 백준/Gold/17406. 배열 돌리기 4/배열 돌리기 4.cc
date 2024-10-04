347e21a31e7c99a69104dcc9702188c2cfb54b70
wannaBpark <leehun456@hanmail.net>
2024-10-04 16:37:43 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 24 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+using ll = long long;
+struct op{
+    int r, c ,s;
+};
+
+int A[60][60];
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int N, M, K;
+vector<op> v;
+vector<int> order;
+int arr[60][60];
+
+
+void print(int r,int c,int s) {
+    printf("------%d %d %d rotation\n", r,c,s);
+    for (int i = r-s; i<=r+s; ++i) {
+        for (int j = c-s; j <=c+s; ++j) {
+            printf("%d ", arr[i][j]);
+        }
+        puts("");
+    }
+    printf("---------------\n");
+}
+static inline int OutOfRange(int sx, int sy, int ex, int ey, int x, int y) {
+    return ((x>ex) || (x<sx) || (y>ey) || (y<sy));
+}
+
+void rotate(int x1, int y1, int x2, int y2) {
+    int& endX = x2; int& startX = x1;
+    int& endY = y2; int& startY = y1;
+    int dir = 0;
+    int tmp = arr[startX][startY];
+    int nx = startX; int ny = startY;
+    while (true) {
+        // printf("to %d %d dir:%d", nx, ny,dir);
+        // nx, ny 는 처음에 항상 유효하다 가정
+        int& dest = arr[nx][ny];
+
+        int IsOut = OutOfRange(x1,y1,x2,y2, nx + dx[dir], ny + dy[dir]);
+        dir += (IsOut == true) ? 1 : 0;
+        if (dir >= 4) break;
+
+        nx = nx + dx[dir]; ny = ny + dy[dir];
+
+
+        int& from = arr[nx][ny];
+        // printf(" from %d %d dest: %d from: %d %d\n", nx, ny, dest, from,dir);
+        dest = from;
+    }
+    arr[startX][startY+1] = tmp;
+}
+
+void operation(int r, int c, int s) {
+    for (int i = 1; i <= s; ++i) {
+        rotate(r-i, c-i, r+i, c+i);
+    }
+    // print(r,c,s);
+}
+
+int calculate() {
+    int ans = 1e10;
+    for (int i = 1; i <= N; ++i) {
+        int sum = 0; 
+        for (int j = 1; j <= M; ++j) {
+            sum += arr[i][j]; 
+        }
+        ans = min(ans, sum);
+    }
+    return ans;
+}
+void copyValue() {
+    for (int i = 1; i<=N; ++i) for(int j =1; j<=M; ++j) arr[i][j] = A[i][j];
+}
+void solve() {
+    int ans = 1e10;
+    do {
+        copyValue();
+
+        for (int i = 0; i < order.size(); ++i) {
+            auto& cur = v[order[i]];
+            operation(cur.r, cur.c, cur.s);
+            // printf("%d %d %d\n", cur.r, cur.c, cur.s);
+        }
+        
+        ans = min(ans, calculate());
+    } while( next_permutation(order.begin(), order.end()) );
+
+    cout << ans;
+}
+
+int main()
+{
+    cin.tie(NULL)->sync_with_stdio(false);
+    
+    cin >> N >> M >> K;
+    for (int i = 1; i<= N; ++i) {
+        for (int j = 1; j <=M; ++j) {
+            cin >> A[i][j];
+        }
+    }
+    for (int i = 0; i < K; ++i) {
+        order.push_back(i);
+        int r,c,s;
+        cin >> r >> c >> s;
+        v.push_back({r,c,s});
+    }
+    solve();
+    return 0;
+}
