ca59fae829e772781de7d81afa26a9cd43994b4f
wannaBpark <leehun456@hanmail.net>
2024-11-07 21:49:16 +0900

    [Gold I] Title: 슈넬치킨 랑데부, Time: 308 ms, Memory: 15108 KB -BaekjoonHub

---
+++ 백준/Gold/28130. 슈넬치킨 랑데부/슈넬치킨 랑데부.cc
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+#define INF 1e9
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+int visited[1010][1010]={0,};
+int Dist[1010][1010];
+int T1[1010][1010]={0,};
+char A[1010][1010];
+int N, M;
+queue<pii> q;
+queue<pii> q_it;
+pii sunwoo;
+int dir = 0;
+int ans = INF;
+static inline int isRange(int i, int j) {
+    return (i>= 0 && i<N && j<M && j>= 0);
+}
+void bfs() {
+    while(!q.empty()) {
+        auto p = q.front(); q.pop();
+        auto& i = p.first; auto& j = p.second;
+        auto& cur = Dist[i][j];
+        visited[i][j] = true;
+        for (int k = 0; k < 4; ++k) {
+            int nx = i + dx[k]; int ny = j + dy[k];
+            if (!isRange(nx, ny) || Dist[nx][ny] == -1 || visited[nx][ny] == true) continue;
+            auto& nxt = Dist[nx][ny];
+            visited[nx][ny] = true;
+            nxt = min(cur + 1, nxt);
+            q.push({nx, ny});
+        }
+    }
+}
+void iteration() {
+    int d = dir;
+    int nx, ny;
+    int x = sunwoo.first; int y = sunwoo.second;
+    int cnt =0;
+    while (visited[x][y]!=true) {
+        if (cnt == Dist[x][y]) ans = min(ans, cnt);
+        nx = x + dx[d]; ny = y + dy[d];
+        if (!isRange(nx, ny)) {
+            ++d; d = d%4;
+            nx = x + dx[d]; ny = y + dy[d];
+        }
+        visited[x][y] = true;
+        x=nx; y = ny;
+        T1[x][y] = ++cnt;
+        // cout << x << " " << y <<" " << T1[x][y] << '\n';
+    }
+    x = sunwoo.first; y = sunwoo.second;
+    d=dir;
+    T1[x][y] = 0;
+    // for (int i =0; i<N; ++i) {
+    //     for (int j =0; j<M; ++j) {
+    //         cout << T1[i][j] << " ";
+    //     }
+    //     cout << '\n';
+    // }
+    while (visited[x][y]==true) {
+        nx = x + dx[d]; ny = y + dy[d];
+        if (!isRange(nx, ny)) {
+            ++d; d = d%4;
+            nx = x + dx[d]; ny = y + dy[d];
+        }
+        int t1 = T1[x][y]; int t2 = Dist[x][y];
+        visited[x][y] = 0;
+        while (!(t1 >= t2)) {
+            t1 += cnt;
+        }
+        if ( (t1-t2) % 2== 0) {
+            //cout << "x, y : " << x << " " << y << '\n';
+            //cout << "t1 t2 : " <<t1 << " " << t2 << '\n';
+            ans = min(ans, t1);
+        }
+        x=nx; y = ny;
+    }
+}
+int main()
+{
+    cin.tie(NULL)->sync_with_stdio(false);
+    cin >> N >> M;
+    for (int i =0; i<N; ++i) {
+        for (int j =0; j<M; ++j) {
+            char c; cin >> c;
+            A[i][j] = c;
+            Dist[i][j] = INF;
+            if (c == 'A') {
+                Dist[i][j] = 0; q.push({i,j});
+            } else if (c=='G') {
+                Dist[i][j] = -1;
+            } else if (c=='B') {
+                sunwoo = {i,j};
+            }
+        }
+    }
+    bfs();
+    for (int i =0; i<N; ++i) {
+        for (int j =0; j<M; ++j) {
+            visited[i][j] = 0;
+            // cout << Dist[i][j] << " ";
+        }
+        // cout << '\n';
+    }
+    if ( sunwoo.first == 0) {
+        dir = 0;
+    } else if (sunwoo.first == N-1) {
+        dir = 2;
+    } else {
+        if (sunwoo.second == M-1) dir = 1;
+        else dir = 3;
+    }
+    iteration();
+    if (ans==INF) ans = -1;
+    cout << ans;
+    return 0;
+}
