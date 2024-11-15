094498381e51b408df8aee71117128fa334aaa33
wannaBpark <leehun456@hanmail.net>
2024-11-15 22:30:40 +0900

    [Silver I] Title: 징검다리 건너기 (large), Time: 40 ms, Memory: 2132 KB -BaekjoonHub

--- 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.cc
+++ 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.cc
@@ -2,18 +2,16 @@
 #define INF 1e9
 using namespace std;
 using ll = long long;
-
-ll A[5010];
+ll D[5004];
+ll A[5004];
 int N;
-ll D[5010];
+// D[cur] = 1번째 돌에서 건널때 cur로 올수 있는 K의 최솟값
 ll dp(int cur) {
-    auto& ret = D[cur];
+    ll& ret = D[cur];
     if (ret!=INF) return ret;
-
     for (int nxt = cur-1; nxt >= 1; --nxt) {
-        auto power = (cur - nxt) * (1 + abs(A[cur] - A[nxt]));
+        ll power = (cur - nxt) * (1 + abs(A[cur] - A[nxt]));
         ll result = max(power,dp(nxt));
-        // cout << "cur and nxt : " << cur << " " << nxt << " pow : " << power << " result : " << result <<'\n';
         ret = min(ret,  result);
     }    
     return ret;
@@ -22,13 +20,8 @@ int main()
 {
     cin.tie(NULL)->sync_with_stdio(false);
     cin >> N;
-    for (int i =1; i<=N; ++i) {
-        cin >> A[i]; D[i] = INF;
-    }
+    for (int i =1; i<=N; ++i) cin >> A[i], D[i] = INF;
     D[1] = 0;
-    cout << dp(N) <<'\n';
-    // for (int i =1; i<=N; ++i) {
-    //     cout << D[i] << " ";
-    // }
+    cout << dp(N);
     return 0;
 }
