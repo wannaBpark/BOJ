9778fc762a79d7d7200b4926410770fe369bfb70
wannaBpark <leehun456@hanmail.net>
2024-11-17 22:45:49 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 8 ms, Memory: 2824 KB -BaekjoonHub

---
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+vector<int> v[10010];
+int depth[10002];
+int par[10002];
+int T, N,A,B,root;
+int p, child;
+void dfs(int cur, int height) {
+    depth[cur] = height;
+    for(auto& nxt : v[cur]) {
+        dfs(nxt, height +1);
+    }
+}
+int LCA(int a, int b) {
+    if (depth[a]<depth[b]) swap(a,b);
+    while((depth[a] > depth[b])) {
+        a=par[a];
+    }
+    while(a != b) {
+        a = par[a]; b = par[b];
+    }
+    return a;
+}
+void solve() {
+    cin >> T;
+    while (T--) {
+        cin >> N;
+        memset(depth, 0, N+1); 
+        memset(par, 0, N+1); 
+        
+        for(int i=1; i<=N; ++i) v[i].clear();
+        for (int i =0; i<N-1; ++i) {
+            cin >> p >> child;
+            v[p].push_back(child); par[child] = p;
+        }
+        cin >> A >> B;
+        for (int i =1; i<=N; ++i) {
+            if (!par[i]) {root = i; break;}
+        }
+        par[root] = root;
+        dfs(root, 0);
+        int ret = LCA(A, B);
+        cout << ret << '\n';
+    }
+}
+int main()
+{
+    cin.tie(NULL)->sync_with_stdio(false);
+    solve();
+    return 0;
+}
