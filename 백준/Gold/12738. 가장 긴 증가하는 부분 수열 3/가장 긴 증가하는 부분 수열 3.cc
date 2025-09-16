d4da8a5bd6edd8e4c5142ee4ac7a958c8d4d0d1d
wannaBpark <leehun456@hanmail.net>
2025-09-17 03:06:26 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 176 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -1,33 +1,32 @@
 #include <bits/stdc++.h>
-using ll = long long;
 using namespace std;
+using pii = pair<int, int>;
+using ll = long long;
 
-int N;
 vector<int> v;
 vector<int> ans;
+int N;
+
 int main()
 {
     cin.tie(NULL)->sync_with_stdio(false);
-    cout.tie(NULL);
-
     cin >> N;
-    for (int i = 0; i<N; ++i) {
-        int a;  cin >> a;
+    for (int i = 0; i < N; ++i) {
+        int a;
+        cin >> a;
         v.push_back(a);
     }
+
     for (auto it = v.begin(); it != v.end(); ++it) {
         int cur = *it;
         int sz = ans.size();
-        auto idx = lower_bound(ans.begin(), ans.end(), cur) - ans.begin();
-        if (idx == sz) ans.push_back(cur);
-        else {
+        int idx = lower_bound(ans.begin(), ans.end(), cur) - ans.begin();
+        if (idx == sz) {
+            ans.push_back(cur);
+        } else {
             ans[idx] = cur;
         }
     }
-    cout << ans.size() ;
-    // << '\n';
-    // for(auto it = ans.begin(); it != ans.end(); ++it) {
-    //     cout << *it << ' ';
-    // }
+    cout << ans.size();
     return 0;
 }
